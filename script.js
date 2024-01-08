044cfb452a0060b875f083c1e04bd9ad50a73453
rioabu <155930404+rioabu@users.noreply.github.com>
2024-01-08 08:49:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const audio =
+document body 
+
+audio.addEventListener('play', ()=> {body.classList.add('play-music');});
+
+audio.addEventListener('pause', ()=> {body.classList.add('play-music');});
